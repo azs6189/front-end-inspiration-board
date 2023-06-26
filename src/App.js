2707c96fc8b4cbf52f2aa731d17142ea84e3a811
Dee Williams <deejmwilliams@gmail.com>
2023-06-26 12:38:06 -0400

    Add planning comments to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import './App.css';
 
 function App() {
+  // Components possibly needed:
+
+  // Board
+  //// Each Board is an <li> element
+  //// Clicking on the name of a board changes the active board
+
+  // Card
+  //// You can click a "like" button to update likes on a card
+  //// Each card is associated with a single board
+
+  // BoardList (under "Boards" heading in demo)
+
+  // CardList (under "Cards for ${}" heading in demo)
+
+  // BoardForm
+  //// Board form has inputs for title and owner, both required
+  //// Submitting the new board form creates a new board
+  //// Board form can be hidden/unhidden with a button
+
+  // CardForm
+  //// Card form has an input for message
+  //// Submitting creates a new card associated with the active board
+
   return <div className='App'></div>;
 }
 
